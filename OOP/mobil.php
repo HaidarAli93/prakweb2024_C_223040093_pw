abcc809d69b24312a5eaf7b8cb6e8ed0ab2016d2
Aco Koca <fgh54@gmx.com>
2024-10-12 05:57:35 +0700

    Tugas 3: Inheritance OOP

--- OOP/mobil.php
+++ OOP/mobil.php
@@ -2,19 +2,34 @@
 
 Class Mobil {
 	public $nama,
-	public $merk,
-	public $warna,
-	public $kecepatanMaksimal,
-	public $jumlahPenumpang;
+		 $merk,
+		 $warna,
+		 $kecepatanMaksimal,
+		 $jumlahPenumpang;
 
 	public function tambahKecepatan() {
-
+		return "Kecepatan Bertambah!";
 	}
 
 	public function kurangiKecepatan() {
-
+		
 	}
 
 	public function gantiTransmisi() {
 
 	}
+}
+
+class MobilSport extends Mobil {
+	public $turbo = false;
+
+	public function jalankanTurbo() {
+		$this->turbo = true;
+		return "Turbo dijalankan!";
+	}
+}
+
+$mobil = new MobilSport();
+echo $mobil->tambahKecepatan();
+echo "<br>";
+echo $mobil->jalankanTurbo();
--- OOP/produk.php
+++ OOP/produk.php
@@ -4,36 +4,71 @@ class Produk {
 	public $judul,
 		$penulis,
 		$penerbit,
-		$harga;
+		$harga,
+		$jmlHal,
+		$wktMain,
+		$type;
 
 	public function __construct( $judul = "judul", $penulis = "penulis",
-		$penerbit = "penerbit", $harga = 0) {
+		$penerbit = "penerbit", $harga = 0, $jmlHal = 0, $wktMain = 0, $tipe) {
 		$this->judul = $judul;
 		$this->penulis = $penulis;
 		$this->penerbit = $penerbit;
 		$this->harga = $harga;
+		$this->jmlHal = $jmlHal;
+		$this->wktMain = $wktMain;
+		$this->tipe = $tipe;
 	}
 
 	public function getLabel() {
 		return "$this->penulis, $this->penerbit";
+	}
+
+	public function getInfoLengkap() {
+		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+		if ($this->tipe == "Komik" ) {
+			$str .= " ~ {$this->jmlHal} Halaman.";
+		} else if ($this->tipe == "Game") {
+			$str .= " ~ {$this->wktMain} Jam.";
+		}
+
+		return $str;
+	}
+
+	public function getInfoProduk() {
+		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+		return $str;
+	}
+}
+
+class Komik extends Produk {
+	public function getInfoProduk() {
+		$str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->jmlHal} Halaman.";
+		return $str;
+	}
+}
+	
+class Game extends Produk {
+	public function getInfoProduk() {
+		$str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->wktMain} Jam.";
+		return $str;
 	}
 }
 
 class CetakInfoProduk {
 	public function cetak(Produk $produk) {
 		$str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+
 		return $str;
 	}
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
-echo "Komik : " . $produk1->getLabel();
+echo $produk1->getInfoProduk();
 echo "<br>";
-echo "Game : " . $produk2->getLabel();
+echo $produk2->getInfoProduk();
 echo "<br>";
-
-$infoProduk1 = new CetakInfoProduk();
-echo $infoProduk1->cetak($produk1);
 ?>
