81b693f91feb3ab964d51cde17de902d24692f74
Aco Koca <fgh54@gmx.com>
2024-10-12 16:02:13 +0700

    Tugas 3: Setter & Getter OOP

--- OOP/produk.php
+++ OOP/produk.php
@@ -1,13 +1,12 @@
 <?php
 
 class Produk {
-	public $judul,
+	private $judul,
 		$penulis,
-		$penerbit;
+		$penerbit,
+		$harga;
 
 	protected $diskon;
-
-	private $harga;
 
 	public function __construct( $judul = "judul", $penulis = "penulis",
 		$penerbit = "penerbit", $harga = 0) {
@@ -17,10 +16,42 @@ class Produk {
 		$this->harga = $harga;
 	}
 
+	public function setJudul($judul) {
+		$this->judul = $judul;
+	}
+
+	public function getJudul() {
+		return $this->judul;
+	}
+
+	public function setPenulis($penulis) {
+		$this->penulis = $penulis;
+	}
+
+	public function getPenulis() {
+		return $this->penulis;
+	}
+
+	public function setPenerbit($penerbit) {
+		$this->penerbit = $penerbit;
+	}
+
+	public function getPenerbit() {
+		return $this->penerbit;
+	}
+
 	public function setDiskon( $diskon ) {
 		$this->diskon = $diskon;
 	}
 
+	public function getDiskon() {
+		return $this->diskon;
+	}
+
+	public function setHarga( $harga ) {
+		$this->harga = $harga;
+	}
+	
 	public function getHarga() {
 		return $this->harga - ($this->harga*$this->diskon/100);
 	}
@@ -89,4 +120,9 @@ echo "<br>";
 
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
+echo "<br>";
+
+$produk1->setPenulis("Sandhika Galih");
+echo $produk1->getPenulis();
+echo "<br>";
 ?>
